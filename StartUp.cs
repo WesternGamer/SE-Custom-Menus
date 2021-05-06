f4f915eec9ae9850bc9d18204c76f47e0696feb3
WesternGamer <Kevnguyen474@gmail.com>
2021-05-05 20:59:46 -0400

    Revert "Revert "Revert "Added menu packs code."""
    This reverts commit e2e0eb3caa39c073b762d9f99afb33a775d4d8e1.

--- StartUp.cs
+++ StartUp.cs
@@ -1,41 +1,18 @@
 ﻿using HarmonyLib;
 using Sandbox.Game;
-using SE_Custom_Menus.Utill;
-using System;
-using System.IO;
-using System.Security.Principal;
+using System.IO;     
 using VRage.FileSystem;
 using VRage.Plugins;
-using VRage.Utils;
-
 public class StartUp : IPlugin
 {
-    public MyMenuPacksFinder List { get; }
-    public MyXmlUtill Config { get; }
     public StartUp()
     {
-        AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
-        MyLog.Default.WriteLine("[SE Custom Menus]: Plugin Loaded");
         Harmony harmony = new Harmony("SE_Custom_Menus");
         harmony.PatchAll();
 
         string menuPacksPath = Path.GetFullPath(Path.Combine(MyFileSystem.UserDataPath, "MenuPacks"));
         if (!Directory.Exists(menuPacksPath))
             Directory.CreateDirectory(menuPacksPath);
-        string tempFilePath = Path.GetFullPath(Path.Combine(MyFileSystem.UserDataPath, "MenuPacks\\Temp"));
-        if (!Directory.Exists(tempFilePath))
-            Directory.CreateDirectory(tempFilePath);
-
-        Config = MyXmlUtill.Load(menuPacksPath);
-        List = new MyMenuPacksFinder(menuPacksPath, Config);
-
-        MyLog.Default.WriteLine("[SE Custom Menus]: Loading config.");
-        Config.Init(List);
-
-        
-            
-            
-
     }
     public void Dispose()
     {
